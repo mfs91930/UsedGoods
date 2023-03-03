373814ba31f8f653d1de13e492690015a395a364
KANG <mfs91930@gmail.com>
2023-03-03 16:41:46 +0900

    Java Spring gradle 프로젝트 생성

---
+++ src/main/java/com/kjj/usedgoods/UsedGoodsApplication.java
@@ -0,0 +1,13 @@
+package com.kjj.usedgoods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsedGoodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsedGoodsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kjj/usedgoods/UsedGoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjj.usedgoods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsedGoodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
